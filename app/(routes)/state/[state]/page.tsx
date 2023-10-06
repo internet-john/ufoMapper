72f8c270b586127b4199ef97cf8127d6f978140c
internet-john <holman.jx@gmail.com>
2023-10-06 12:36:07 -0400

    display dynamic data

--- app/(routes)/state/[state]/page.tsx
+++ app/(routes)/state/[state]/page.tsx
@@ -1,19 +1,23 @@
 const sightings = require("../../../api/ufo_sightings.json");
 
+import stateMatcher from "@/app/api/utils/stateMatcher";
+
 const getSightings = (state: string) => {
-  const data = sightings.filter((event) => event.city === "germantown");
+  const selectedState = stateMatcher(state);
+  const data = sightings.filter(
+    (event: UfoSighting) => event.state === selectedState
+  );
 
   return data;
 };
 
 const StatePage = ({ params }: { params: { state: string } }) => {
   const data = getSightings(params.state);
-
   return (
     <div>
       <h1>{params.state}</h1>
       <div>
-        {data.map((sighting) => (
+        {data.map((sighting: UfoSighting) => (
           <div>{sighting.comments}</div>
         ))}
       </div>
